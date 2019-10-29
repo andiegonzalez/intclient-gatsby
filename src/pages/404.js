e79a79c1fda70b0a5b54cb240edc3f72fba088f5
Andrea Gonzalez A <a.gzzart@gmail.com>
2019-10-29 12:38:25 -0600

    fixed issue

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,18 @@
 import React from "react"
-
+import { ThemeProvider, CSSReset } from "@chakra-ui/core"
+import { customTheme } from "../theme.js"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+  <ThemeProvider theme={customTheme}>
+    <CSSReset />
+    <Layout>
+      <SEO title="404: Not found" />
+      <h1>NOT FOUND</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </Layout>
+  </ThemeProvider>
 )
 
 export default NotFoundPage
